512f5ef13d3390dfc1044e485546626edc554278
Juan Perez <jperez4432@gmail.com>
2021-06-29 15:22:49 -0500

    completed page counter

---
+++ src/main/java/PageCounter.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet (name = "PageCounter", urlPatterns = "/count")
+public class PageCounter extends HttpServlet {
+    int countViews = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        if(req.getParameter("reset") != null)
+            countViews = 0;
+        else
+            countViews += 1;
+        out.println("<h1>The count is currently at " + countViews + "</h1>");
+    }
+}
