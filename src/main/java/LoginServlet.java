5e6dbda06cbeea551c82926305dbca6b64e074d8
Juan Perez <jperez4432@gmail.com>
2021-07-08 14:33:49 -0500

    completed sessions exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,59 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+
+        request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean isAdmin = username.equals("admin") && password.equals("password");
+        boolean isLoggedIn = username.equals("juan") && password.equals("password");
 
-        if (validAttempt) {
+        HttpSession session = request.getSession();
+
+        if (isLoggedIn) {
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
-        } else {
-            response.sendRedirect("/login");
         }
+
+
+//        if (isAdmin) {
+//            session.setAttribute("isAdmin", true);
+//            response.sendRedirect("/secret-admin-page");
+//        } else {
+//            response.sendRedirect("/WEB-INF/login");
+//        }
+
+        //set the session object to a variable on the HttpSession class
+//        HttpSession session = request.getSession();
+//
+//        String darkMode = request.getParameter("color");
+//
+//        session.setAttribute("darkM0ode", darkMode);
+//
+//        //on a different servlet(profile page)
+//        //with our getAttribute method, our return is always a generic Object type
+//        //therefore we need to explicitly cast the object to the correct type
+//        session.getAttribute("darkMode");
+//        String darkSelected = (String) session.getAttribute("darkMode");
+//
+//        session.removeAttribute("darkMode");
+//
+//        //destroys the session
+//        session.invalidate();
+//
+
     }
 }
