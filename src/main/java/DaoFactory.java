f3287df9ea4bda0c1e725977e961aa0813498db4
Juan Perez <jperez4432@gmail.com>
2021-07-09 15:37:24 -0500

    troubleshooting

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,10 @@
 public class DaoFactory {
     private static Ads adsDao;
-
+    private static Config config = new Config();
     public static Ads getAdsDao() {
+        Config config = new Config();
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
